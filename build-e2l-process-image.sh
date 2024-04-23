18af39447d8bda888553f544b9e0d6eb59dee890
StefanoMilani <milani@diag.uniroma1.it>
2024-04-23 11:44:29 +0200

    ADD build script for process
    Signed-Off-By: Stefano Milani <milani@diag.uniroma1.it>

---
+++ build-e2l-process-image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "Please, provide the image tag"
+    exit 1
+fi
+
+image_tag=$1
+
+docker build -t e2l-process:$image_tag -f e2l-process.Dockerfile ./e2l-process/pyspark_app2/
