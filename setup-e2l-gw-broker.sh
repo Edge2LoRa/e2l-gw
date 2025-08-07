e84898421d94de23abe1e4ab377c47f8f7a127a3
Nil <tahmasebi@arpsoft.it>
2025-08-07 16:54:51 +0200

    Refactor frame counters into shared FrameCounter struct

--- setup-e2l-gw-broker.sh
+++ setup-e2l-gw-broker.sh
@@ -13,7 +13,7 @@ then
 else
     echo "e2l gw broker data directory already exixts"
 fi
-sudo chown 1883:1883 ${DATA_DIRECTORY} -R
+#sudo chown 1883:1883 ${DATA_DIRECTORY} -R
 
 mkdir ${LOG_DIRECTORY} > /dev/null 2>&1
 ret=$?
@@ -26,6 +26,6 @@ else
     echo "e2l gw broker log directory already exixts"
 fi
 chmod o+r ${LOG_FILE_PATH}
-sudo chown 1883:1883 ${LOG_DIRECTORY} -R
+#sudo chown 1883:1883 ${LOG_DIRECTORY} -R
 
-docker build -t e2l-gw-broker:v1.0.0 -f e2l-gw-broker.Dockerfile .
+#docker build -t e2l-gw-broker:v1.0.0 -f e2l-gw-broker.Dockerfile .
