2ab8339b4c988d67d3f125ac1860087f79c233ca
Nil <tahmasebi@arpsoft.it>
2025-09-12 17:58:27 +0200

    Increase both counters(rx_ho_frames,proc_frames) inside the Some(payload)

--- e2l-parser/src/e2l_mqtt_client/mod.rs
+++ e2l-parser/src/e2l_mqtt_client/mod.rs
@@ -438,11 +438,10 @@ pub(crate) mod e2l_mqtt_client {
                             let ret = e2l_crypto
                                 .handover_callback(topic.to_string(), msg_str.to_string());
                             std::mem::drop(e2l_crypto);
-                            let mut counters = FRAME_COUNTERS.lock().unwrap();
-                            counters.rx_ho_frames += 1;
                             match ret {
-
                                 Some(payload) => {
+                                    let mut counters = FRAME_COUNTERS.lock().unwrap();
+                                    counters.rx_ho_frames += 1;
                                     counters.proc_frames +=1;
                                     self.publish_to_process(payload).await;
                                 },
